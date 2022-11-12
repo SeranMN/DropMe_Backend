54fe8ffc2aee1581f88d1090032b7a1ee4038f65
SeranMN <88319801+SeranMN@users.noreply.github.com>
2022-11-12 21:27:25 +0530

    fix issues in index.js

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const connectDB = require("./config/config");
 const Bus = require('./api/Bus.api')
 const Login = require('./api/Login.api')
 const TimeTable = require('./api/TimeTable.api');
 const User = require('./api/User.api')
 const Trip = require('./api/Trip.api')
 const Route = require('./api/Route.api')
+const connectDB = require("./config/config");
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 
-connectDB()
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello Node!");
+});
+
 app.use("/bus", Bus())
 app.use('/login', Login());
 app.use('/timetable',TimeTable())
 app.use('/user', User())
 app.use('/trip', Trip())
 app.use('/route', Route())
-app.use('/time',TimeTable())
+app.use('/time', TimeTable())
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
